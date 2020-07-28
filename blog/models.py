e2305d8e1f03e2e7c2555f19859b9a61cb606901
rafid <rafidmostafa13@gmail.com>
2020-07-28 12:45:03 +0600

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+import datetime
+
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
+
